b4f5a3d7f131d5434ec2b7a30381217227fa12da
Gatlin Newhouse <gatlin.newhouse@gmail.com>
2025-02-05 06:22:02 +0000

    Fix some function name errors

--- lib/iov_iter.c
+++ lib/iov_iter.c
@@ -204,8 +204,9 @@ static int copyin_no_dfcache(void *to, const void __user *from, size_t n)
 	if (should_fail_usercopy())
 		return n;
 	if (access_ok(from, n)) {
-		instrument_copy_from_user(to, from, n);
+		instrument_copy_from_user_before(to, from, n);
 		n = raw_copy_from_user_no_dfcache(to, from, n);
+		instrument_copy_from_user_after(to, from, n);
 	}
 	return n;
 }
