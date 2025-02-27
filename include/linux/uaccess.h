13c654e6aaaee395bce39158ce16ef4f16a62d9f
Gatlin Newhouse <gatlin.newhouse@gmail.com>
2025-02-27 16:37:55 +0000

    Remove define, keep export?

--- include/linux/uaccess.h
+++ include/linux/uaccess.h
@@ -147,7 +147,6 @@ __copy_to_user(void __user *to, const void *from, unsigned long n)
  *
  * Rust code always uses the extern definition.
  */
-#if !defined(INLINE_COPY_FROM_USER) || defined(CONFIG_RUST)
 static inline __must_check unsigned long
 _inline_copy_from_user(void *to, const void __user *from, unsigned long n)
 {
@@ -168,30 +167,12 @@ _inline_copy_from_user(void *to, const void __user *from, unsigned long n)
 		memset(to + (n - res), 0, res);
 	return res;
 }
-#ifdef CONFIG_SAFEFETCH
-static inline __must_check unsigned long
-_copy_from_user_no_dfcache(void *to, const void __user *from, unsigned long n)
-{
-	unsigned long res = n;
-	might_fault();
-	if (!should_fail_usercopy() && likely(access_ok(from, n))) {
-		instrument_copy_from_user_before(to, from, n);
-		res = raw_copy_from_user_no_dfcache(to, from, n);
-		instrument_copy_from_user_after(to, from, n, res);
-	}
-	if (unlikely(res))
-		memset(to + (n - res), 0, res);
-	return res;
-}
-#endif
-
-#else
 extern __must_check unsigned long
 _copy_from_user(void *, const void __user *, unsigned long);
+
 #ifdef CONFIG_SAFEFETCH
 extern __must_check unsigned long
 _copy_from_user_no_dfcache(void *, const void __user *, unsigned long);
-#endif
 #endif
 
 static inline __must_check unsigned long
