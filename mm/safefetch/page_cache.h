db493a5f9bf77bd6cca7c5ec512518339d4d3bcd
Gatlin Newhouse <gatlin.newhouse@gmail.com>
2025-05-14 20:58:36 +0000

    Run `clang-format` on `mm/safefetch` files to fix easy formatting errors

--- mm/safefetch/page_cache.h
+++ mm/safefetch/page_cache.h
@@ -29,108 +29,112 @@ extern struct kmem_cache *df_metadata_cache, *df_storage_cache;
 /* #warning Using User Supplied Cache for Storage */
 #endif
 
-extern size_t safefetch_metadata_cache_size, safefetch_storage_cache_size; 
+extern size_t safefetch_metadata_cache_size, safefetch_storage_cache_size;
 extern uint8_t safefetch_slow_path_order;
 void df_init_page_alloc_array(void);
-void df_resize_page_caches(size_t _metadata_size, size_t _storage_size, uint8_t _order);
+void df_resize_page_caches(size_t _metadata_size, size_t _storage_size,
+			   uint8_t _order);
 
 #ifndef PAGE_SHIFT
 #define PAGE_SHIFT 12
 #endif
 
-enum df_cache_type {
-   METADATA, 
-   STORAGE    
-};
+enum df_cache_type { METADATA, STORAGE };
 
-static __always_inline void *df_allocate_metadata_chunk(void){
-    return kmem_cache_alloc(df_metadata_cache, GFP_ATOMIC);
+static __always_inline void *df_allocate_metadata_chunk(void)
+{
+	return kmem_cache_alloc(df_metadata_cache, GFP_ATOMIC);
 }
 
-static __always_inline void *df_allocate_storage_chunk(void){
-    return kmem_cache_alloc(df_storage_cache, GFP_ATOMIC);
+static __always_inline void *df_allocate_storage_chunk(void)
+{
+	return kmem_cache_alloc(df_storage_cache, GFP_ATOMIC);
 }
 
-static __always_inline void df_release_metadata_chunk(void *obj){ 
-    kmem_cache_free(df_metadata_cache, obj);
-    return;
+static __always_inline void df_release_metadata_chunk(void *obj)
+{
+	kmem_cache_free(df_metadata_cache, obj);
+	return;
 }
 
-static __always_inline void df_release_storage_chunk(void *obj){
-    kmem_cache_free(df_storage_cache, obj);
-    return;
+static __always_inline void df_release_storage_chunk(void *obj)
+{
+	kmem_cache_free(df_storage_cache, obj);
+	return;
 }
 
-static __always_inline void* df_allocate_page(u8 cache_type){
-   switch (cache_type){
-          case METADATA:
-                      return df_allocate_metadata_chunk();
-          case STORAGE:
-                      return df_allocate_storage_chunk();
-   }
-   return 0;
+static __always_inline void *df_allocate_page(u8 cache_type)
+{
+	switch (cache_type) {
+	case METADATA:
+		return df_allocate_metadata_chunk();
+	case STORAGE:
+		return df_allocate_storage_chunk();
+	}
+	return 0;
 }
 
-static __always_inline void df_free_page(void *obj, u8 cache_type){
-   switch (cache_type){
-          case METADATA:
-                      df_release_metadata_chunk(obj);
-                      return;
-          case STORAGE:
-                      df_release_storage_chunk(obj);
-                      return;
-   }
-   return;
+static __always_inline void df_free_page(void *obj, u8 cache_type)
+{
+	switch (cache_type) {
+	case METADATA:
+		df_release_metadata_chunk(obj);
+		return;
+	case STORAGE:
+		df_release_storage_chunk(obj);
+		return;
+	}
+	return;
 }
 
-static __always_inline void* df_allocate_chunk(struct kmem_cache *cache){
+static __always_inline void *df_allocate_chunk(struct kmem_cache *cache)
+{
 #if defined(SAFEFETCH_DEBUG) && defined(SAFEFETCH_DEBUG_LEAKS)
-    unsigned long iflags;
-    spin_lock_irqsave(&allocations_lock, iflags);
-    global_allocations++;
-    DF_ALLOCATIONS(current)++;
-    spin_unlock_irqrestore(&allocations_lock, iflags);
+	unsigned long iflags;
+	spin_lock_irqsave(&allocations_lock, iflags);
+	global_allocations++;
+	DF_ALLOCATIONS(current)++;
+	spin_unlock_irqrestore(&allocations_lock, iflags);
 #endif
-    gfp_t flags = unlikely(in_atomic()) ? GFP_ATOMIC : GFP_KERNEL; 
-    return kmem_cache_alloc(cache, flags);
+	gfp_t flags = unlikely(in_atomic()) ? GFP_ATOMIC : GFP_KERNEL;
+	return kmem_cache_alloc(cache, flags);
 }
 
-static __always_inline void df_free_chunk(struct kmem_cache *cache, void *obj){
+static __always_inline void df_free_chunk(struct kmem_cache *cache, void *obj)
+{
 #if defined(SAFEFETCH_DEBUG) && defined(SAFEFETCH_DEBUG_LEAKS)
-    unsigned long iflags;
-    spin_lock_irqsave(&allocations_lock, iflags);
-    global_allocations--;
-    DF_ALLOCATIONS(current)--;
-    spin_unlock_irqrestore(&allocations_lock, iflags);
+	unsigned long iflags;
+	spin_lock_irqsave(&allocations_lock, iflags);
+	global_allocations--;
+	DF_ALLOCATIONS(current)--;
+	spin_unlock_irqrestore(&allocations_lock, iflags);
 #endif
-    kmem_cache_free(cache, obj);
+	kmem_cache_free(cache, obj);
 }
 
-static  __always_inline void* df_allocate_chunk_slowpath(size_t size){
+static __always_inline void *df_allocate_chunk_slowpath(size_t size)
+{
 #if defined(SAFEFETCH_DEBUG) && defined(SAFEFETCH_DEBUG_LEAKS)
-    unsigned long iflags;
-    spin_lock_irqsave(&allocations_lock, iflags);
-    global_allocations++;
-    DF_ALLOCATIONS(current)++;
-    spin_unlock_irqrestore(&allocations_lock, iflags);
+	unsigned long iflags;
+	spin_lock_irqsave(&allocations_lock, iflags);
+	global_allocations++;
+	DF_ALLOCATIONS(current)++;
+	spin_unlock_irqrestore(&allocations_lock, iflags);
 #endif
-    gfp_t flags = unlikely(in_atomic()) ? GFP_ATOMIC : GFP_KERNEL; 
-    return kmalloc(size, flags); 
+	gfp_t flags = unlikely(in_atomic()) ? GFP_ATOMIC : GFP_KERNEL;
+	return kmalloc(size, flags);
 }
 
-static  __always_inline void df_free_chunk_slowpath(void *obj){
+static __always_inline void df_free_chunk_slowpath(void *obj)
+{
 #if defined(SAFEFETCH_DEBUG) && defined(SAFEFETCH_DEBUG_LEAKS)
-    unsigned long iflags;
-    spin_lock_irqsave(&allocations_lock, iflags);
-    global_allocations--;
-    DF_ALLOCATIONS(current)--;
-    spin_unlock_irqrestore(&allocations_lock, iflags);
+	unsigned long iflags;
+	spin_lock_irqsave(&allocations_lock, iflags);
+	global_allocations--;
+	DF_ALLOCATIONS(current)--;
+	spin_unlock_irqrestore(&allocations_lock, iflags);
 #endif
-    kfree(obj);
+	kfree(obj);
 }
-
-
-
-
 
 #endif
--- mm/safefetch/safefetch_static_keys.c
+++ mm/safefetch/safefetch_static_keys.c
@@ -2,7 +2,6 @@
 #include <linux/swap.h>
 #include "page_cache.h"
 
-
 DEFINE_STATIC_KEY_FALSE(safefetch_copy_from_user_key);
 DEFINE_STATIC_KEY_FALSE(safefetch_hooks_key);
 DEFINE_STATIC_KEY_FALSE(safefetch_adaptive_key);
@@ -16,145 +15,149 @@ extern uint8_t SAFEFETCH_ADAPTIVE_WATERMARK;
 
 volatile int copy_from_user_key_ctrl = 0;
 volatile int hooks_key_ctrl = 0;
-volatile int defense_config_ctrl  = -1;
+volatile int defense_config_ctrl = -1;
 volatile int storage_regions_ctrl = -1;
 volatile uint8_t adaptive_watermark_ctrl = -1;
 
-
-static ssize_t hooks_show(struct kobject *kobj, 
-                struct kobj_attribute *attr, char *buf)
+static ssize_t hooks_show(struct kobject *kobj, struct kobj_attribute *attr,
+			  char *buf)
 {
-    return sprintf(buf, "%d", hooks_key_ctrl);
+	return sprintf(buf, "%d", hooks_key_ctrl);
 }
-static ssize_t hooks_store(struct kobject *kobj, 
-                struct kobj_attribute *attr,const char *buf, size_t count)
+static ssize_t hooks_store(struct kobject *kobj, struct kobj_attribute *attr,
+			   const char *buf, size_t count)
 {
-        int val;
-        sscanf(buf,"%d",&val);
-        // WARNING. Only enable the hooks once (disabling this after enabling
-        // it will cause race conditions or missing cleanups).
-        if ((hooks_key_ctrl != val) && (val == 0 || val == 1)){
-           hooks_key_ctrl = val;
-           if (hooks_key_ctrl){
-              static_branch_enable(&safefetch_hooks_key);
-           } else {
-              static_branch_disable(&safefetch_hooks_key);
-           }
-        }
-        
+	int val;
+	sscanf(buf, "%d", &val);
+	// WARNING. Only enable the hooks once (disabling this after enabling
+	// it will cause race conditions or missing cleanups).
+	if ((hooks_key_ctrl != val) && (val == 0 || val == 1)) {
+		hooks_key_ctrl = val;
+		if (hooks_key_ctrl) {
+			static_branch_enable(&safefetch_hooks_key);
+		} else {
+			static_branch_disable(&safefetch_hooks_key);
+		}
+	}
 
-        return count;
+	return count;
 }
 
-static ssize_t copy_from_user_show(struct kobject *kobj, 
-                struct kobj_attribute *attr, char *buf)
+static ssize_t copy_from_user_show(struct kobject *kobj,
+				   struct kobj_attribute *attr, char *buf)
 {
-    return sprintf(buf, "%d", copy_from_user_key_ctrl);
+	return sprintf(buf, "%d", copy_from_user_key_ctrl);
 }
-static ssize_t copy_from_user_store(struct kobject *kobj, 
-                struct kobj_attribute *attr,const char *buf, size_t count)
+static ssize_t copy_from_user_store(struct kobject *kobj,
+				    struct kobj_attribute *attr,
+				    const char *buf, size_t count)
 {
-        int val;
-        sscanf(buf,"%d",&val);
-        // Nothing to do if we already have it activated or deactivated.
-        if ((copy_from_user_key_ctrl != val) && (val == 0 || val == 1)){
-           copy_from_user_key_ctrl = val;
-           if (copy_from_user_key_ctrl){
-              static_branch_enable(&safefetch_copy_from_user_key);
-           } else {
-              static_branch_disable(&safefetch_copy_from_user_key);
-           }
-        }
-        return count;
-}
-
-static ssize_t defense_config_ctrl_show(struct kobject *kobj, 
-                struct kobj_attribute *attr, char *buf)
-{
-    return sprintf(buf, "%d", defense_config_ctrl);
+	int val;
+	sscanf(buf, "%d", &val);
+	// Nothing to do if we already have it activated or deactivated.
+	if ((copy_from_user_key_ctrl != val) && (val == 0 || val == 1)) {
+		copy_from_user_key_ctrl = val;
+		if (copy_from_user_key_ctrl) {
+			static_branch_enable(&safefetch_copy_from_user_key);
+		} else {
+			static_branch_disable(&safefetch_copy_from_user_key);
+		}
+	}
+	return count;
 }
 
-// Warning. This function must be called with safefetch_copy_from_user_key
-// disabled. Previously the assumption was to also disable the hook key 
-// but this causes race conditions. So, after enabling the hook key once
-// never disable it (we cannot toggle back to baseline in other words).
-static ssize_t defense_config_ctrl_store(struct kobject *kobj, 
-                struct kobj_attribute *attr,const char *buf, size_t count)
+static ssize_t defense_config_ctrl_show(struct kobject *kobj,
+					struct kobj_attribute *attr, char *buf)
 {
-        int val;
-        sscanf(buf,"%d",&val);
-
-        if (val == defense_config_ctrl){
-            return count;
-        }
-
-        if (val == 0) { // Linked list configuration
-            static_branch_disable(&safefetch_adaptive_key);
-            static_branch_disable(&safefetch_rbtree_key);
-        } else if (val == 1) { // RB-Tree configuration.
-            static_branch_disable(&safefetch_adaptive_key);
-            static_branch_enable(&safefetch_rbtree_key);
-        } else if (val == 2) { // Adaptive configuration
-            static_branch_disable(&safefetch_rbtree_key);
-            static_branch_enable(&safefetch_adaptive_key);
-        }
-
-        defense_config_ctrl = val;
-
-        return count;
-}
-
-static ssize_t storage_regions_ctrl_show(struct kobject *kobj, 
-                struct kobj_attribute *attr, char *buf)
-{
-    return sprintf(buf, "%d", storage_regions_ctrl);
+	return sprintf(buf, "%d", defense_config_ctrl);
 }
 
 // Warning. This function must be called with safefetch_copy_from_user_key
-// disabled. Previously the assumption was to also disable the hook key 
+// disabled. Previously the assumption was to also disable the hook key
 // but this causes race conditions. So, after enabling the hook key once
 // never disable it (we cannot toggle back to baseline in other words).
-static ssize_t storage_regions_ctrl_store(struct kobject *kobj, 
-                struct kobj_attribute *attr,const char *buf, size_t count)
+static ssize_t defense_config_ctrl_store(struct kobject *kobj,
+					 struct kobj_attribute *attr,
+					 const char *buf, size_t count)
 {
+	int val;
+	sscanf(buf, "%d", &val);
 
-        size_t metadata, storage;
-        uint8_t order = 0;
-        sscanf(buf,"%ld %ld %hhd",&metadata, &storage, &order);
+	if (val == defense_config_ctrl) {
+		return count;
+	}
 
-        printk("Supplied METADATA: %ld and STORAGE: %ld and ORDER: %d\n", metadata, storage, order);
-        
-        df_resize_page_caches(metadata, storage, order);
+	if (val == 0) { // Linked list configuration
+		static_branch_disable(&safefetch_adaptive_key);
+		static_branch_disable(&safefetch_rbtree_key);
+	} else if (val == 1) { // RB-Tree configuration.
+		static_branch_disable(&safefetch_adaptive_key);
+		static_branch_enable(&safefetch_rbtree_key);
+	} else if (val == 2) { // Adaptive configuration
+		static_branch_disable(&safefetch_rbtree_key);
+		static_branch_enable(&safefetch_adaptive_key);
+	}
 
-        return count;
+	defense_config_ctrl = val;
+
+	return count;
 }
 
-static ssize_t adaptive_watermark_ctrl_show(struct kobject *kobj, 
-                struct kobj_attribute *attr, char *buf)
+static ssize_t storage_regions_ctrl_show(struct kobject *kobj,
+					 struct kobj_attribute *attr, char *buf)
 {
-    return sprintf(buf, "%hhd", adaptive_watermark_ctrl);
+	return sprintf(buf, "%d", storage_regions_ctrl);
 }
 
 // Warning. This function must be called with safefetch_copy_from_user_key
-// disabled. Previously the assumption was to also disable the hook key 
+// disabled. Previously the assumption was to also disable the hook key
 // but this causes race conditions. So, after enabling the hook key once
 // never disable it (we cannot toggle back to baseline in other words).
-static ssize_t adaptive_watermark_ctrl_store(struct kobject *kobj, 
-                struct kobj_attribute *attr,const char *buf, size_t count)
+static ssize_t storage_regions_ctrl_store(struct kobject *kobj,
+					  struct kobj_attribute *attr,
+					  const char *buf, size_t count)
 {
-        adaptive_watermark_ctrl = 0;
+	size_t metadata, storage;
+	uint8_t order = 0;
+	sscanf(buf, "%ld %ld %hhd", &metadata, &storage, &order);
 
-        sscanf(buf,"%hhd",&adaptive_watermark_ctrl);
+	printk("Supplied METADATA: %ld and STORAGE: %ld and ORDER: %d\n",
+	       metadata, storage, order);
 
-             
+	df_resize_page_caches(metadata, storage, order);
+
+	return count;
+}
+
+static ssize_t adaptive_watermark_ctrl_show(struct kobject *kobj,
+					    struct kobj_attribute *attr,
+					    char *buf)
+{
+	return sprintf(buf, "%hhd", adaptive_watermark_ctrl);
+}
+
+// Warning. This function must be called with safefetch_copy_from_user_key
+// disabled. Previously the assumption was to also disable the hook key
+// but this causes race conditions. So, after enabling the hook key once
+// never disable it (we cannot toggle back to baseline in other words).
+static ssize_t adaptive_watermark_ctrl_store(struct kobject *kobj,
+					     struct kobj_attribute *attr,
+					     const char *buf, size_t count)
+{
+	adaptive_watermark_ctrl = 0;
+
+	sscanf(buf, "%hhd", &adaptive_watermark_ctrl);
+
 #ifdef SAFEFETCH_FLOATING_ADAPTIVE_WATERMARK
-        if (adaptive_watermark_ctrl && (((adaptive_watermark_ctrl + 1) & adaptive_watermark_ctrl) == 0)) {
-            SAFEFETCH_ADAPTIVE_WATERMARK = adaptive_watermark_ctrl;
-            printk("Supplied ADAPTIVE watermark %hhd\n", SAFEFETCH_ADAPTIVE_WATERMARK);
-        }
+	if (adaptive_watermark_ctrl &&
+	    (((adaptive_watermark_ctrl + 1) & adaptive_watermark_ctrl) == 0)) {
+		SAFEFETCH_ADAPTIVE_WATERMARK = adaptive_watermark_ctrl;
+		printk("Supplied ADAPTIVE watermark %hhd\n",
+		       SAFEFETCH_ADAPTIVE_WATERMARK);
+	}
 #endif
 
-        return count;
+	return count;
 }
 
 #if 0
@@ -210,68 +213,85 @@ static ssize_t defense_full_ctrl_store(struct kobject *kobj,
 }
 #endif
 
-struct kobj_attribute copy_from_user_key_ctrl_attr = __ATTR(copy_from_user_key_ctrl, 0660, copy_from_user_show, copy_from_user_store);
+struct kobj_attribute copy_from_user_key_ctrl_attr =
+	__ATTR(copy_from_user_key_ctrl, 0660, copy_from_user_show,
+	       copy_from_user_store);
 
-struct kobj_attribute hooks_key_ctrl_attr = __ATTR(hooks_key_ctrl, 0660, hooks_show, hooks_store);
+struct kobj_attribute hooks_key_ctrl_attr =
+	__ATTR(hooks_key_ctrl, 0660, hooks_show, hooks_store);
 
-struct kobj_attribute defense_config_ctrl_attr = __ATTR(defense_config_ctrl, 0660, defense_config_ctrl_show, defense_config_ctrl_store);
+struct kobj_attribute defense_config_ctrl_attr =
+	__ATTR(defense_config_ctrl, 0660, defense_config_ctrl_show,
+	       defense_config_ctrl_store);
 
-struct kobj_attribute storage_regions_ctrl_attr = __ATTR(storage_regions_ctrl, 0660, storage_regions_ctrl_show, storage_regions_ctrl_store);
+struct kobj_attribute storage_regions_ctrl_attr =
+	__ATTR(storage_regions_ctrl, 0660, storage_regions_ctrl_show,
+	       storage_regions_ctrl_store);
 
-struct kobj_attribute adaptive_watermark_ctrl_attr = __ATTR(adaptive_watermark_ctrl, 0660, adaptive_watermark_ctrl_show, adaptive_watermark_ctrl_store);
+struct kobj_attribute adaptive_watermark_ctrl_attr =
+	__ATTR(adaptive_watermark_ctrl, 0660, adaptive_watermark_ctrl_show,
+	       adaptive_watermark_ctrl_store);
 
-void init_safefetch_skey_layer(void) {
-    //This Function will be called from Init function
-    /*Creating a directory in /sys/kernel/ */
-    struct kobject *kobj_ref = kobject_create_and_add("dfcacher_keys", kernel_kobj);
+void init_safefetch_skey_layer(void)
+{
+	//This Function will be called from Init function
+	/*Creating a directory in /sys/kernel/ */
+	struct kobject *kobj_ref =
+		kobject_create_and_add("dfcacher_keys", kernel_kobj);
 
-    if (!kobj_ref){
-        printk(KERN_INFO"[SafeFetch-keys] Cannot create kobj_ref......\n");
-        goto end;
-    }
+	if (!kobj_ref) {
+		printk(KERN_INFO
+		       "[SafeFetch-keys] Cannot create kobj_ref......\n");
+		goto end;
+	}
 
-    if(sysfs_create_file(kobj_ref, &copy_from_user_key_ctrl_attr.attr)){
-      printk(KERN_INFO"[SafeFetch-keys] Cannot create sysfs file for copy_from_user control......\n");
-      goto fail_copy_key;
-    }
+	if (sysfs_create_file(kobj_ref, &copy_from_user_key_ctrl_attr.attr)) {
+		printk(KERN_INFO
+		       "[SafeFetch-keys] Cannot create sysfs file for copy_from_user control......\n");
+		goto fail_copy_key;
+	}
 
-    if(sysfs_create_file(kobj_ref, &hooks_key_ctrl_attr.attr)){
-      printk(KERN_INFO"[SafeFetch-keys] Cannot create sysfs file for hook control......\n");
-      goto fail_hooks_key;
-    }
+	if (sysfs_create_file(kobj_ref, &hooks_key_ctrl_attr.attr)) {
+		printk(KERN_INFO
+		       "[SafeFetch-keys] Cannot create sysfs file for hook control......\n");
+		goto fail_hooks_key;
+	}
 
-    if(sysfs_create_file(kobj_ref, &defense_config_ctrl_attr.attr)){
-      printk(KERN_INFO"[SafeFetch-keys] Cannot create sysfs file for defense control......\n");
-      goto fail_defense_key;
-    }
+	if (sysfs_create_file(kobj_ref, &defense_config_ctrl_attr.attr)) {
+		printk(KERN_INFO
+		       "[SafeFetch-keys] Cannot create sysfs file for defense control......\n");
+		goto fail_defense_key;
+	}
 
-    if(sysfs_create_file(kobj_ref, &storage_regions_ctrl_attr.attr)){
-      printk(KERN_INFO"[SafeFetch-keys] Cannot create sysfs file for storage region control......\n");
-      goto fail_storage_key;
-    }
+	if (sysfs_create_file(kobj_ref, &storage_regions_ctrl_attr.attr)) {
+		printk(KERN_INFO
+		       "[SafeFetch-keys] Cannot create sysfs file for storage region control......\n");
+		goto fail_storage_key;
+	}
 
-    if(sysfs_create_file(kobj_ref, &adaptive_watermark_ctrl_attr.attr)){
-      printk(KERN_INFO"[SafeFetch-keys] Cannot create sysfs file for storage region control......\n");
-      goto fail_adaptive_key;
-    }
+	if (sysfs_create_file(kobj_ref, &adaptive_watermark_ctrl_attr.attr)) {
+		printk(KERN_INFO
+		       "[SafeFetch-keys] Cannot create sysfs file for storage region control......\n");
+		goto fail_adaptive_key;
+	}
 
+	printk(KERN_INFO
+	       "[SafeFetch-keys] Successfully created references to control DFCACHER......\n");
 
-    printk(KERN_INFO"[SafeFetch-keys] Successfully created references to control DFCACHER......\n");
+	return;
 
-    return;
-
-fail_adaptive_key: 
-    sysfs_remove_file(kernel_kobj, &adaptive_watermark_ctrl_attr.attr); 
+fail_adaptive_key:
+	sysfs_remove_file(kernel_kobj, &adaptive_watermark_ctrl_attr.attr);
 fail_storage_key:
-    sysfs_remove_file(kernel_kobj, &storage_regions_ctrl_attr.attr); 
+	sysfs_remove_file(kernel_kobj, &storage_regions_ctrl_attr.attr);
 fail_defense_key:
-    sysfs_remove_file(kernel_kobj, &defense_config_ctrl_attr.attr);  
+	sysfs_remove_file(kernel_kobj, &defense_config_ctrl_attr.attr);
 fail_hooks_key:
-    sysfs_remove_file(kernel_kobj, &hooks_key_ctrl_attr.attr);  
+	sysfs_remove_file(kernel_kobj, &hooks_key_ctrl_attr.attr);
 fail_copy_key:
-    sysfs_remove_file(kernel_kobj, &copy_from_user_key_ctrl_attr.attr);
-    kobject_put(kobj_ref);
+	sysfs_remove_file(kernel_kobj, &copy_from_user_key_ctrl_attr.attr);
+	kobject_put(kobj_ref);
 
 end:
-    return;
+	return;
 }
